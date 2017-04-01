8e13e3c150a418f5a4e6898280f9a0ad777ff433
Pyramid Head <jason@sociablelabs.com>
2017-03-31 18:28:47 -0700

    Update poison.sh

--- poison.sh
+++ poison.sh
@@ -18,24 +18,24 @@ while [ "$iteration" -le "$permute" ]; do
 	#imthisguy=(`python randyline.py usemeagent`)
 
 	# test for domain presence
-  testhost="$dictword.$tld"
-  read hostout <<< $(host $testhost)
+	testhost="$dictword.$tld"
+	read hostout <<< $(host $testhost)
 
-  # if DNS record returned, curl www, else ignore
-  now=$(date +"%r")
-  if [[ $hostout == *"has address"* ]]; then
-  	mycurl=("-s -A '"'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14'"' www.$testhost")
-  	echo "domain "$testhost" found at "$now"." >> foundit.log
-  	curl -s $mycurl >> /dev/null
-  	sleep 30
-  elif [[ $hostout == *"is an alias for"* ]]; then
-   	mycurl=("-s -A '"'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14'"' www.$testhost")
-  	echo "domain "$testhost" found at "$now"." >> foundit.log
-  	curl -s $mycurl >> /dev/null
-  	sleep 30
-  elif [[ $hostout == *"not found"* ]]; then
-  	sleep 5
-fi
+	# if DNS record returned, curl www, else ignore
+	now=$(date +"%r")
+	if [[ $hostout == *"has address"* ]]; then
+  		mycurl=("-s -A '"'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14'"' www.$testhost")
+  		echo "domain "$testhost" found at "$now"." >> foundit.log
+  		curl -s $mycurl >> /dev/null
+  		sleep 30
+	elif [[ $hostout == *"is an alias for"* ]]; then
+   		mycurl=("-s -A '"'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14'"' www.$testhost")
+  		echo "domain "$testhost" found at "$now"." >> foundit.log
+  		curl -s $mycurl >> /dev/null
+  		sleep 30
+  	elif [[ $hostout == *"not found"* ]]; then
+  		sleep 5
+	fi
 
 iteration=$(($iteration + 1))
 done
